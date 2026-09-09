36d7649f51dce491b6d2ada94b4fb8f980a2b567
WANG ZIXUAN <1209068419@qq.com>
2026-09-09 18:02:20 +0900

    Convert iOS app to WebView wrapper

--- ios/NodeulgilWork/NodeulgilWork/NodeulgilWorkApp.swift
+++ ios/NodeulgilWork/NodeulgilWork/NodeulgilWorkApp.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct NodeulgilWorkApp: App {
-    @StateObject private var store = AppStore()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(store)
         }
     }
 }
